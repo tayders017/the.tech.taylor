356c7d918e22f0666ee802fcd6328a2007c30472
tayders017 <30681484+tayders017@users.noreply.github.com>
2024-06-28 21:14:24 -0500

    Create initials.py

---
+++ projects/codecademy/Learn_Python_3/Block Letters/initials.py
@@ -0,0 +1,10 @@
+#Initials = T  M  N  T
+#Fun fact = TMNT = Teenage Mutant Ninja Turtles
+
+print("TTTTT  M   M  N   N  TTTTT")
+print("  T    MM MM  NN  N    T  ")
+print("  T    MM MM  N N N    T  ")
+print("  T    M M M  N  NN    T  ")
+print("  T    M   M  N   N    T  ")
+print("  T    M   M  N   N    T  ")
+print("  T    M   M  N   N    T  ")
