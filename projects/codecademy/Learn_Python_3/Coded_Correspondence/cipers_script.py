64781d0823ebc12cc55bdefb01ac6c299e5ac34d
tayders017 <30681484+tayders017@users.noreply.github.com>
2024-07-04 19:12:29 -0500

    Create cipers_script.py

---
+++ projects/codecademy/Learn_Python_3/Coded_Correspondence/cipers_script.py
@@ -0,0 +1,22 @@
+#Off-Platform Project: Coded Correspondence
+#Project overview:
+#In this off-platform project, you will be exchanging coded communications with your pen pal, Vishal.
+#The two of you challenge each other by sending coded messages that you will have to use your Python skills to decipher!
+#Put your programming skills to the test with these fun cryptography puzzles.
+#
+#You and your pen pal, Vishal, have been exchanging letters for some time now.
+#Recently, he has become interested in cryptography and the two of you have started sending encoded messages within your letters.
+#In this project, you will use your Python skills to decipher the messages you receive and to encode your own responses!
+#Put your programming skills to the test with these fun cryptography puzzles.
+#Here is his most recent letter:
+# - Hey there! How have you been? I've been great! I just learned about this really cool type of cipher called a Caesar Cipher.
+# - Here's how it works: You take your message, something like "hello" and then you shift all of the letters by a certain offset. 
+# - For example, if I chose an offset of 3 and a message of "hello", I would encode my message by shifting each letter 3 places to the left with respect to the alphabet.
+# - So "h" becomes "e", "e" becomes "b", "l" becomes "i", and "o" becomes "l". Then I have my encoded message, "ebiil"!
+# - Now I can send you my message and the offset and you can decode it by shifting each letter 3 places to the right.
+# - The best thing is that Julius Caesar himself used this cipher, that's why it's called the Caesar Cipher! Isn't that so cool!
+# - Okay, now I'm going to send you a longer encoded message that you have to decode yourself!
+# - xuo jxuhu! jxyi yi qd unqcfbu ev q squiqh syfxuh. muhu oek qrbu je tusetu yj? y xefu ie! iudt cu q cuiiqwu rqsa myjx jxu iqcu evviuj!
+# - This message has an offset of 10. Can you decode it?
+
+
