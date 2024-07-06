dae2dd5024fbd189bd724e9ea0dd410e24923280
tayders017 <30681484+tayders017@users.noreply.github.com>
2024-07-06 02:27:27 -0500

    Update scrabble.py

--- projects/codecademy/Learn_Python_3/Scrabble/scrabble.py
+++ projects/codecademy/Learn_Python_3/Scrabble/scrabble.py
@@ -6,15 +6,13 @@
 #
 #
 #Additional feature to add for practice:
-# - play_word():
+# -DONE- play_word():
 # - update_point_totals():
 # - allow lowercase input to letter_to_points
 #Additional features I came up with:
 # - Add option to account for double/triple word scores
 # - Add option to account for empty rack bonus
 # - Advanced additional features: create gui of board that tracks all moves
-
-
 letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
 
@@ -38,8 +36,13 @@ def score_word(word):
 #browniw_points = score_word("BROWNIE")
 #print(browniw_points)
 
-#Create dictionary to map players to a list of words they have played
-player_to_words = {"player1": ["BLUE", "TENNIS", "EXIT"], "wordNerd": ["EARTH", "EYES", "MACHINE"], "Lexi Con": ["ERASER", "BELLY", "HUSKY"], "Prof Reader": ["ZAP", "COMA", "PERIOD"]}
+#Create dictionary to map players to a list of words played
+player_to_words = {
+  "player1": ["BLUE", "TENNIS", "EXIT"], 
+  "wordNerd": ["EARTH", "EYES", "MACHINE"], 
+  "Lexi Con": ["ERASER", "BELLY", "HUSKY"], 
+  "Prof Reader": ["ZAP", "COMA", "PERIOD"]
+}
 
 #Create dictionary for player_to_points
 player_to_points = {}
@@ -48,9 +51,12 @@ for player, words in player_to_words.items():
   for word in words:
     player_points += score_word(word)
   player_to_points[player] = player_points
-#print(player_to_points)
+print(player_to_points)
 
+#-----FURTHER PRACTICE-----#
+
+#Create play_word() function to intake player & word and add to list of words played
 def play_word(player, word):
   player_to_words[player].append(word)
 play_word("player1", "CODE")
-print(play_word)
+print(player_to_words)
