5aebed474410fbea4538b959f865a725966703fb
tayders017 <30681484+tayders017@users.noreply.github.com>
2024-07-08 23:55:12 -0500

    Create restaurant_script.py

---
+++ projects/codecademy/Learn_Python_3/Basta_Fazoolin/restaurant_script.py
@@ -0,0 +1,5 @@
+#Project overview:
+#In this restauranteur project you’ll design software that handles operations for a chain of restaurants!
+#You’ve started a position as the lead programmer for the family-style Italian restaurant Basta Fazoolin’ with My Heart.
+#The restaurant has been doing fantastically and seen a lot of growth lately.
+#You’ve been hired to keep things organized.
