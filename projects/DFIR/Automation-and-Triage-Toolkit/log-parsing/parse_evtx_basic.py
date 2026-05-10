f59c39c7f492c7e298c30249f4c58f16dc3601d8
tayders017 <30681484+tayders017@users.noreply.github.com>
2026-05-10 17:24:44 -0500

    Update parse_evtx_basic.py

--- projects/DFIR/Automation-and-Triage-Toolkit/log-parsing/parse_evtx_basic.py
+++ projects/DFIR/Automation-and-Triage-Toolkit/log-parsing/parse_evtx_basic.py
@@ -1,12 +1,3 @@
-"""
-==============================================================================
-PARSING FAILED LOGON SECURITY EVENT SCRIPT
-==============================================================================
-PURPOSE: Parses a Windows Security.evtx file for Event ID 4625 (Failed Logon).
-REQUIREMENTS: pip install python-evtx lxml
-==============================================================================
-"""
-
 from Evtx.Evtx import Evtx
 
 with Evtx("Security.evtx") as log:
